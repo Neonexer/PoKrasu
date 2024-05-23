263142ab3dd16bfc9da7ac22ea8448b0182e792d
max <Neonexer12@gmail.com>
2024-05-23 15:36:00 +0700

    added app restaurants

--- Project/urls.py
+++ Project/urls.py
@@ -21,6 +21,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('restaurants.urls'), name='restaurants'),
     path('articles/', include('articles.urls'), name='articles'),
     path('login/', include('user_auth.urls'), name='login'),
+    # path('restaurants/', include('restaurants.urls'), name='restaurants'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
