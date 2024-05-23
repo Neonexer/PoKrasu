894e3b7c318e50961b34153e4002c5d1dea69bbf
max <Neonexer12@gmail.com>
2024-05-23 15:47:28 +0700

    added app restaurants

--- Project/settings.py
+++ Project/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'Project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'restaurants/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
