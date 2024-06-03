0eee52980242cd4d5be7cd0cae95961a72db5f21
max <Neonexer12@gmail.com>
2024-06-03 17:30:46 +0700

    working pagination

--- articles/urls.py
+++ articles/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='articles'),
+    path('page/', views.index, name='articles_page_1'),
+    path('page/<int:page>', views.index, name='articles_page'),
     path('<int:article_id>/', views.article, name='article'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,11 +1,17 @@
+from django.core.paginator import Paginator
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Article
 
 # Create your views here.
-def index(request):
+def index(request, page=1):
+    paginator = Paginator(Article.objects.all(), 5)
+    page = paginator.page(page)
     articles = Article.objects.order_by('-date')
-    return render(request, 'articles/articles_home.html', {'articles': articles})
+    return render(request, 'articles/articles_home.html', {
+        'paginator': paginator,
+        'page': page,
+    })
 
 
 def article(request, article_id):
