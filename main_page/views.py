1de680dc72270e9491e666743ac7a8b1e1f36eaf
max <Neonexer12@gmail.com>
2024-06-04 13:02:29 +0700

    deleted filter

--- main_page/views.py
+++ main_page/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
-from restaurants.models import Restaurant
+from restaurants.models import Restaurant, Review
 from articles.models import Article
 # Create your views here.
 def index(request):
     restaurants = Restaurant.objects.order_by('?')[:4]
     articles = Article.objects.all()[:6]
 
-    return render(request, 'main_page/main_page.html', {'restaurants': restaurants, 'articles': articles})
+    restaurant_reviews = {}
+    for i in restaurants:
+        review_count = Review.objects.filter(restaurant=i).count()
+        restaurant_reviews[i] = review_count
+
+    return render(request, 'main_page/main_page.html', {
+        'restaurants': restaurants,
+        'articles': articles,
+        'restaurant_reviews': restaurant_reviews
+    })
